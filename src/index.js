ea0f9466e69a1904a75524b29886236dbbcbbe29
Lavelian <98335265+Lavelian@users.noreply.github.com>
2022-08-03 10:14:15 +0300

    instal biblio

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  inputel: document.querySelector('#search-box'),
+  ulEl: document.querySelectorAll('.country-list'),
+  divEl: document.querySelectorAll('.country-info'),
+};
+
+function onInput(e) {
+  fetchCountries(e.target.value.trim())
+    .then(r => r.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+refs.inputel.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
